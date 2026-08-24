0113e94e11dd0a1bf521b69c8b4d8e91e800047d
AetherCode <aethercode.core@gmail.com>
2026-08-24 18:18:07 +0300

    v2026.9.0: replace template with unique implementation

--- main.go
+++ main.go
@@ -1,13 +1,43 @@
-
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"runtime"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println("os:", runtime.GOOS)
-	fmt.Println("arch:", runtime.GOARCH)
-	fmt.Println("version:", runtime.Version())
+	if len(os.Args) < 3 {
+		fmt.Println("Usage: code-translator <source-file> <target-lang>")
+		fmt.Println("Supported: python, javascript, rust")
+		return
+	}
+	f, err := os.Open(os.Args[1])
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer f.Close()
+	lang := os.Args[2]
+	scanner := bufio.NewScanner(f)
+	translated := 0
+	fmt.Printf("Code Translator: -> %s\n\n", strings.Title(lang))
+	for scanner.Scan() {
+		line := scanner.Text()
+		switch lang {
+		case "python":
+			line = strings.ReplaceAll(line, "func ", "def ")
+			line = strings.ReplaceAll(line, "fmt.Println", "print")
+		case "javascript":
+			line = strings.ReplaceAll(line, "func ", "function ")
+			line = strings.ReplaceAll(line, "fmt.Println", "console.log")
+		case "rust":
+			line = strings.ReplaceAll(line, "func ", "fn ")
+			line = strings.ReplaceAll(line, "fmt.Println", "println!")
+		}
+		fmt.Println(line)
+		translated++
+	}
+	fmt.Printf("\nTranslated %d lines to %s\n", translated, lang)
 }
